794e8973500d97b9a9c5a654d689252499ef2174
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-06 21:50:28 +0000

    Using fstring to print string

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,2 @@
-#!/bin/bash
-python3 - c "print('\"Programming is like building a multilingual puzzle')"
+#!/usr/bin/python3
+python3 -c "print('\"Programming is like building a multilingual puzzle')"
