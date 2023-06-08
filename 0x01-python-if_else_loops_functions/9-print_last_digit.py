f0555db7969d524be8f42312aedc372dea8f07c3
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-08 00:58:35 +0000

    Pycodestyle requirement

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last = number % 10
-    if number < 0:
-        neglast = (-1 * number) % 10  # Get last number
-        print("{}".format(neglast), end='')  # Handles negatives
-        return neglastv  # As per task
-    else:
-        print("{}".format(last), end='')
+    last = abs(number) % 10
+    print("{}".format(last), end='')
+    return last
