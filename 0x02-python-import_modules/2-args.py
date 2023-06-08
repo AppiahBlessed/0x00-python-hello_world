6ede8a4cf451fdeb1da3a6190f9d44a8e2cb3199
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-08 21:25:40 +0000

    Print arguements passed at runtime

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 2-args.py
+
+import sys
+# Dont execute when exported as a module
+if __name__ == "__main__":
+    argslist = sys.argv
+    arg_len = len(argslist) - 1  # Get lenght or num or arguements
+    if arg_len == 1:
+        print("{:d} arguement:".format(arg_len))
+    elif arg_len == 0:
+        print("{:d} arguements".format(arg_len))
+    else:
+        print("{:d} arguements:".format(arg_len))
+    for i in range(1, arg_len + 1):
+        print("{}: {}".format(i, argslist[i]))
