3bb33cd9fb136811efd70004f13f593c93967455
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-08 00:24:08 +0000

    Convert to upper case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ''
+    for char in (str):  # Loop through character by chracter
+        if ord(char) >= 97 and ord(char) <= 122:  # Check for lower case
+            newchar = ord(char) - 32  # convert to upper
+            result += chr(newchar)  # add capitalized character to new string
+        else:
+            result += char  # if already upper, add to new string
+    print(result)
