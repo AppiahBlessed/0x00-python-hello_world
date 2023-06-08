044e1f5c53f6306d56a99e6b507f739343d60f51
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-08 21:28:32 +0000

    Just a dot can make you go mad

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if arg_len == 1:
         print("{:d} arguement:".format(arg_len))
     elif arg_len == 0:
-        print("{:d} arguements".format(arg_len))
+        print("{:d} arguements.".format(arg_len))
     else:
         print("{:d} arguements:".format(arg_len))
     for i in range(1, arg_len + 1):
