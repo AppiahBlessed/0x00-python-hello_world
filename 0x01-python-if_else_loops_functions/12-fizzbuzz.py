615b81667b93005ce5f1ff209081ed0de3e191ae
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-08 01:32:58 +0000

    Pycodestyle on FizzBuzz File

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,12 @@
+#!/usr/bin/python3
+# 12-fizzbuzz.py
 def fizzbuzz():
-    for num in range(1, 101):
-        if num % 3 == 0 and num % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif num % 3 == 0:
-            print("Fizz", end=" ")
-        elif num % 5 == 0:
-            print("Buzz", end=" ")
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print(num, end=" ")
-# Calling the fizzbuzz function
-fizzbuzz()
-
+            print("{} ".format(number), end="")
