7b7ddf1480ced35145710b5e21574b855a4f72a0
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-11 13:41:57 +0000

    Reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 3-print_reversed_list_integer.py
-
+# Reversed the srting
 def print_reversed_list_integer(my_list=[]):
-    mynew_list = my_list[::-1]  # Reversed string through slicing
+    mynew_list = my_list[::-1]
     for i in mynew_list:
         print("{:d}".format(i))
