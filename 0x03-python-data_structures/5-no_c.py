290e3f629f1ccd09373d779b0ec08ed4a1fd9b51
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-11 13:33:58 +0000

    Fix loop error

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # 5-no_c.py
 # Loop through, if you find c then take the
-# letters before and after it then add them together
+# Create a new string but without the sad character to beremoved
 
 def no_c(my_string):
-    for idx, value in enumerate(my_string):
-        if value == 'c' or value == 'C':
-            newstring = my_string[:idx] + my_string[idx + 1:]
-            return newstring
+    newstring = ''
+    for value in my_string:
+        if value != 'c' and value != 'C':
+            newstring += value
+    return newstring
