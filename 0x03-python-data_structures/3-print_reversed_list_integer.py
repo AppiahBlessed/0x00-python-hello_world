0fc68990ebd5622105715822289c24aee8122708
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-11 12:29:33 +0000

    Recuirement met

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,4 @@
 def print_reversed_list_integer(my_list=[]):
     mynew_list = my_list[::-1]  # Reversed string through slicing
     for i in mynew_list:
-        print("{:d}".format(i))
+        print(i)
