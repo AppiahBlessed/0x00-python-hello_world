6c3c6c7f529c7fc6d59783d86aaff510913585b0
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-14 23:29:35 +0000

    Pycodestyle Requirement met

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 # 0-square_matrix_simple.py
-def power (num):
+def power(num):
     return num * num
+
 
 def square_matrix_simple(matrix=[]):
     new_list = list(map(lambda numb: list(map(power, numb)), matrix))
     return new_list
-
-
