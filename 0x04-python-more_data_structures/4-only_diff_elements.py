671adcbb3b95840ec92c2c84f9122cedc9d837cc
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-15 01:15:36 +0000

    Remove common element

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,7 +6,7 @@ def only_diff_elements(set_1, set_2):
     for i in set_1:
         if i not in set_2:
             exclusive.add(i)
-        for j in set_2:
-            if j not in set_1:
-                exclusive.add(j)
+        for i in set_2:
+            if i not in set_1:
+                exclusive.add(i)
     return exclusive
