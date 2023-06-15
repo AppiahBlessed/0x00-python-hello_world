7cbe17202b9dd29334bdb706a3c594e9c5190c9b
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-15 15:20:49 +0000

    Meet pycodestyle requirement

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,7 +4,7 @@
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for i in a_dictionary:
-        new_dict[i] = a_dictionary[i]  # Create new dictionary with old contents
+        new_dict[i] = a_dictionary[i]  # Create new dictionary
     for i in new_dict:
         new_dict[i] = new_dict[i] * 2  # Multiply each element by 2
     return new_dict
