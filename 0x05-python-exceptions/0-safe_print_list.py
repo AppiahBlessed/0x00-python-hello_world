6a0867c651c28a17773c842ba53a1fd841f937bd
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-26 18:57:59 +0000

    Handle IndexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,11 +5,11 @@ def safe_print_list(my_list=[], x=0):
     count = 0
 
     for i in my_list:
-        count += 1;  # Get the length of list
+        count += 1  # Get the length of list
     try:
         for i in range(x):
             print(my_list[i], end='')
-    except :
-            pass  # Dont print any error
+    except Exception:
+        pass  # Dont print any error
     print()  # Print new line
     return count
