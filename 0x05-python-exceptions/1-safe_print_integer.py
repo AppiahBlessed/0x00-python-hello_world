e9ea5e94255a07c313ba0eea327469c8192df04e
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-26 19:16:21 +0000

    Check value for integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:  # Ignore error
+    except Exception:  # Ignore error
         pass
     if isinstance(value, int):  # Check instance of integer
         return True
