615e2cc88e84c849750a9c254acadbea2292e46d
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-26 19:41:50 +0000

    Meet Pycodestyle Requirement

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,12 @@
 # 2-safe_print_list_integers.py
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0;
+    count = 0
     try:
         for i in range(x):
             if (isinstance(my_list[i], int)):
                 print("{:d}".format(my_list[i]), end='')
-                count +=1  # Number of elemnt printed
+                count += 1  # Number of elemnt printed
     except ValueError:  # In order to skip non int items quietly
         pass
     except IndexError:  # Raise default error
