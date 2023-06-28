916c3288f376d2b32c79cca527b4e0b9686b62d2
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-28 20:37:04 +0000

    Set class attribute for Square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+"""Writes a class Square"""
+
+
+class Square:
+    """Defines a class square"""
+
+    def __init__(self, size):
+        """Main entry method
+            and below are the Arguements
+            size in this case
+        """
+        self.__size = size
