702c8eef147db99ab73aa86776739e866549f498
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-28 20:17:31 +0000

    Added Documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,4 +2,8 @@
 # 0-square.py
 
 class Square:
-    """This is a class that is meant to be empty.
+    """This is a class that is meant to be empty
+        param1: The first parameter.
+        param1: The first parameter.
+        param1: The first parameter.
+    """.
