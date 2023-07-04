a29dbdf63601ad0c579a18f759f3f396faa8b72a
AppiahBlessd <kwabenadicey@gmail.com>
2023-07-04 09:09:37 +0000

    Added perimeter and area method

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+'''0-rectangle.py'''
+
+
+class Rectangle:
+    '''Defines rectangle'''
+
+    def __init__(self, width=0, height=0):
+        '''Initialise instance'''
+        '''Instance Arguments'''
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        '''Width method'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''Sets rectangles width'''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        '''Height method'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''Sets rectangles width'''
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        '''Defines the area of the rectangle'''
+        return self.width * self.height
+
+    def perimeter(self):
+        '''Finds the permeter of the rectangle'''
+        if self.width == 0 and self.height == 0:
+            perim = 0
+        else:
+            perim = 2 * (self.width + self.height)
+        return perim
+
+    def __str__(self):
+        '''String representation of object'''
+        return f"Rectangle (width={self.__width}, height={self.__height})"
