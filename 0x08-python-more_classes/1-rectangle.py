b4f12c0c742072444f6d44ad7d6ba835f6d8fd6a
AppiahBlessd <kwabenadicey@gmail.com>
2023-07-04 08:58:27 +0000

    Added __str__ method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         '''Initialise instance'''
         '''Instance Arguments'''
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
