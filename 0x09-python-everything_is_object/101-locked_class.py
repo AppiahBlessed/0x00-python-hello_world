35a5d42459621bcb43e02887ecda9b3b7046b732
AppiahBlessd <kwabenadicey@gmail.com>
2023-07-05 13:57:10 +0000

    Advance task

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Have some control over instance sttribute creation
+Dont create instance attribute if its not first_name
+'''
+
+class LockedClass:
+    def __setattr__(self, name, value):
+        if name == 'first_name':
+            self.__dict__[name] = value
+        else:
+            raise AttributeError(
+                    f"'LockedClass' object has no attribute '{name}'"
+            )
