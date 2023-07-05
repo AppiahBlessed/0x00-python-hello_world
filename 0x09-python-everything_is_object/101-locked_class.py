0df44e02fb5249b6e7019b68fa9cc5fa5a488c32
AppiahBlessd <kwabenadicey@gmail.com>
2023-07-05 14:02:41 +0000

    Advance task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,6 +5,7 @@ Dont create instance attribute if its not first_name
 
 
 class LockedClass:
+    '''Have some control over instance sttribute creation'''
     def __setattr__(self, name, value):
         if name == 'first_name':
             self.__dict__[name] = value
