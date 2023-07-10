00ac0c575b74543ab91c02e2dba6ebf0482676d5
AppiahBlessd <kwabenadicey@gmail.com>
2023-07-10 20:41:59 +0000

    Sort list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Lists the elements in a sorted format'''
+
+
+class MyList(list):
+    '''Class is a subclass of the in built list class'''
+    def print_sorted(self):
+        '''Sorts the output'''
+        output = sorted(self)
+        print(output)
