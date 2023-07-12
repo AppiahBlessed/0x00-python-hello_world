5a4e061689eb9c852432dbcc2395506546ebf17c
AppiahBlessd <kwabenadicey@gmail.com>
2023-07-12 22:46:37 +0000

    Append to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def append_write(filename="", text=""):
+    '''Function is meant to append to either an
+        existing file or create one where it doesnt exit
+        Return:
+        Returns the number of added characters
+    '''
+    with open(filename, 'a', encoding='utf-8,') as f:
+        return f.write(text)
