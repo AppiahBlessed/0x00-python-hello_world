ed2e284ee4d09a71b48affda904ef93e2a82e42f
AppiahBlessed <kwabenadicey@gmail.com>
2023-09-11 18:51:37 +0000

    Semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -13,7 +13,7 @@ const num = parseInt(arg3);
 // Checks if arguement is not a number an uses '!' to negate
 
 if (!isNaN(num)) {
-  console.log('My number is: ' + num);
+  console.log('My number: ' + num);
 } else {
   console.log('Not a number');
 }
