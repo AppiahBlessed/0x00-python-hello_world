6ec12202e4c9a6a5c6ee3ad2933dbed9629c688a
AppiahBlessed <kwabenadicey@gmail.com>
2023-09-11 19:20:27 +0000

    Print array list

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+ * This file script prints multiple lines
+ */
+
+let i;
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (i = 0; i < 3; i++) {
+  console.log(arr[i]);
+}
