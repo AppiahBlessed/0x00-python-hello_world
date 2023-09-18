77b4a99864a5ab111cb99455be234460049a685c
AppiahBlessed <kwabenadicey@gmail.com>
2023-09-18 01:16:44 +0000

    Safe filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+ This is a pytthon script that writes takes in an argument
+ and displays all values in the states table of hbtn_0e_0_usa
+ where name matches the argument.
+
+ But prevents injection
+
+"""
+
+
+def main():
+    import sys
+    import MySQLdb
+
+    # Get command line argument passed
+    u = sys.argv[1]
+    pd = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Establish the connection
+    with MySQLdb.connect(host='localhost', port=3306, user=u, password=pd,
+                         db=db_name) as connect, connect.cursor() as cursor:
+        # Execute query using the format string safe from sql injection
+        cursor.execute("""SELECT * FROM states WHERE name
+                        LIKE BINARY %s
+                        ORDER BY states.id ASC""", (state_name, ))
+
+        # Display results
+        output = cursor.fetchall()
+        # Get all names that begin with capital N
+        for i in output:
+            print(i)
+
+
+# Module should not automatically run when imported
+if __name__ == "__main__":
+    main()
