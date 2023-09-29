3b7e73456f31dc1deeacf854af59913743fe3c9a
AppiahBlessed <kwabenadicey@gmail.com>
2023-09-29 00:00:40 +0000

    make executable

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This bash script requests to a url through http and returns the bytes
-curl -sI '$1' | grep -i 'content-length:' | awk '{print $2}
+curl -sI "$1" | grep -i 'content-length:' | awk '{print $2}'
