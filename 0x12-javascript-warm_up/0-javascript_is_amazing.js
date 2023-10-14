9433bb10120a6204cb3b3f36b0de67d8dc66e37e
AppiahBlessed <125456069+AppiahBlessed@users.noreply.github.com>
2023-10-14 10:42:09 +0000

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-/*
- * This is a Javascript script that prints a string
- */
-const myVar = 'JavaScript is amazing';
-
-console.log(`${myVar}`);
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-/*
- * This file script concatenates two strings
- */
-
-console.log(String(process.argv[2]) + ' is ' + String(process.argv[3]));
