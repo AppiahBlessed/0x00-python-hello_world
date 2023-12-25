6c697c9abf492de1616ff81a388d6575c7965345
AppiahBlessed <kwabenadicey@gmail.com>
2023-12-25 12:26:29 +0000

    DELETE request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display body of GET request. the if statement isnt necessay but why not>
-[ "$(curl -sI "$1" | awk 'NR==1{print $2}')" == "200" ] && curl -sL "$1"
+# Display body of GET request. the if statement isnt necessay but why not?
+curl -sL "$1"
