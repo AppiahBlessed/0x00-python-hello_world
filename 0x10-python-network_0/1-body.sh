bd724e606a2a65fcff5dc434e2d9f7a2fadf6503
AppiahBlessed <kwabenadicey@gmail.com>
2023-12-25 12:14:54 +0000

    Change flag

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display body of GET request
-[ "$(curl -sI "$1" | awk 'NR==1{print $2}')" == "200" ] && curl -s $1
+# Display body of GET request. the if statement isnt necessay but why not>
+[ "$(curl -sI "$1" | awk 'NR==1{print $2}')" == "200" ] && curl -sL "$1"
