3f8ca10944130a40db44eb1959ebd3af3750b8f7
AppiahBlessed <kwabenadicey@gmail.com>
2023-12-25 12:47:37 +0000

    Allowed methods with curl

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script gets all the aloowed methods in a request to a website
-curl -sIX DELETE "$1" | grep "Allow" | cut -d " " -f 2-
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
