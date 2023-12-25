a06b352c832777585405f55f7403998980f31853
AppiahBlessed <kwabenadicey@gmail.com>
2023-12-25 13:58:33 +0000

    POST Method

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script uses POST to send data to a site
-curl -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
