93c4c000b02207b5287e0c93e607baf495b13ec1
AppiahBlessed <kwabenadicey@gmail.com>
2023-12-27 11:42:23 +0000

    Documented module

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-import urllib.request
-"""
-    This script fetches a url and displays it in a formatted way
-    It fetches the url and disp[lays the type of data returned (ie class obj).
-    Displays the decoded byte form of the content for more human readable out
+"""This script fetches a url and displays it in a formatted way
+-  It fetches the url and disp[lays the type of data returned (ie class obj).
+-  Displays the decoded byte form of the content for more human readable out
 """
 if __name__ == "__main__":
+    import urllib.request
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
         output = resp.read()
         print("Body response:")
