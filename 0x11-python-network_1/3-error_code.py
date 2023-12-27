55a607a253cf879d667dffcf9d3420b4ae165424
AppiahBlessed <kwabenadicey@gmail.com>
2023-12-27 17:32:06 +0000

    HTTPError status code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This script displays the status code of a request
+- urllib.error.HTTPError
+"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    from urllib.error import HTTPError
+
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as resp:
+            output = resp.read()
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
