7d15ca31a2095d133d450fab2958c87d3162cbbb
AppiahBlessed <kwabenadicey@gmail.com>
2023-12-27 12:28:54 +0000

    Tabulation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
         output = resp.read()
         print("Body response:")
-        print(f"    - type: {type(output)}")
-        print(f"    - content: {output}")
-        print(f"    - utf8 content: {output.decode('utf-8')}")
+        print(f"\t- type: {type(output)}")
+        print(f"\t- content: {output}")
+        print(f"\t- utf8 content: {output.decode('utf-8')}")
