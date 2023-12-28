800d2ebf4bec924aec180bb2cde80b90f36fe994
AppiahBlessed <kwabenadicey@gmail.com>
2023-12-28 11:45:33 +0000

    Requests module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetch webpage using the requests module
+- What's my status?
+"""
+
+if __name__ == "__main__":
+    import requests
+    
+    url = "https://alx-intranet.hbtn.io/status"
+    resp = requests.get(url).text
+    print("Body response:")
+    print("\t- type: {}".format(type(resp)))
+    print("\t- content: {}".format(resp))
