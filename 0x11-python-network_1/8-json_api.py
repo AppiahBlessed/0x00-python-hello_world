1106e0b2cb2599168e5ea6353073a154878014d6
AppiahBlessed <kwabenadicey@gmail.com>
2023-12-28 15:32:23 +0000

    Search API-JSON validation

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This script sends a post request with a ariable
+-   To search
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) < 2:
+        letter = ""
+    else:
+        letter = sys.argv[1]
+    data = {"q": letter}
+    resp = requests.post(url, data=data)
+    
+    # Just to catch any JSON error
+    try:
+        json_data = resp.json()
+        # if the json data is empty or valid 
+        if json_data and isinstance(json_data, (list, dict)):
+            # get the id
+            id_data = json_data.get("id")
+            # get the name
+            name_data = json_data.get("name")
+            print(f"[{id_data}] {name_data}")
+        # If data is empty
+        elif not json_data:
+            print("No result")
+        # If it is not a valid json data
+        elif not isinstance(json_data, (list, dict)):
+            print("Not a valid JSON")
+    # Catch exception
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
+
