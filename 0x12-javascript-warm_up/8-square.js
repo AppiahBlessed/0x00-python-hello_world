546d7dcee2ab1d12dab35936703c0b42f6368ea8
Appiah Blessed <kwabenadicey@gmail.com>
2024-01-30 14:05:21 +0000

    Adherence to semmistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,9 +6,9 @@
 const num = parseInt(process.argv[2]);
 
 if (isNaN(num) || process.argv[2]  === undefined) {
-	console.log("Missing size");
+  console.log('Missing size');
 } else {
-	for (let i = 0; i < num; i++) {
-		console.log('X'.repeat(num));
-	}
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
 }
