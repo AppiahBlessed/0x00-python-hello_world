177c664e2f2ce420b826a88b793670552d8bf2a4
Appiah Blessed <kwabenadicey@gmail.com>
2024-01-30 20:57:53 +0000

    Buil-up on Square class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
-/**
-* This is a new class, square, that inherits from Rectangle class
-*/
 
-class Square extends Rectangle(){
-// Declaring the class
-	constructor(size){
-		super(w, h);
-		this.size = size;
-	}
+const square = require('./5-square.js');
 
-// Prints the square
-	charPrint(c){
-		if (c){
-			for (let i = 0; i < this.size; i++){
-				console.log('C'.repeat(this.size));
-			}
-		} else {
-			for (let i = 0; i < this.size; i++){
-				console.log('X'.repeat(this.size));
-			}
-		  }
-	};
+class Square extends square {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (C) {
+    if (C === undefined) {
+      C = 'X'
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += C;
+      }
+      console.log(s);
+    }
+  }
+}
+module.exports = Square;
