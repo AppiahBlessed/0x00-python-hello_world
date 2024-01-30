0fd2e082196d49d62bd26a8feddd12dba6c0b9fc
Appiah Blessed <kwabenadicey@gmail.com>
2024-01-30 13:38:30 +0000

    Correct output

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const arg1 = process.argv[2];
 
 if (!isNaN(parseFloat(arg1))) {
-  iconst value = parseInt(arg1);
+  const value = parseInt(arg1);
   console.log(`My number: ${value}`);
 } else {
   console.log('Not a number');
