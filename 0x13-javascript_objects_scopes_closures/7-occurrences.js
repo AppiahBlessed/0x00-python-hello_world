68be488405809c522deb9ff1c8471c74f6073b7d
Appiah Blessed <kwabenadicey@gmail.com>
2024-01-30 21:13:17 +0000

    Count occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -4,10 +4,11 @@
 */
 
 exports.nbOccurences = function (list, searchElement) {
-let count = 0;
-// Loop to find element
-	for (let i = 0; i <= list.length; list++){
-		if (searchElement === list[i]);
-			count++;
-	}
-return (count);
+  let count = 0;
+  for (let i = 0; i < list.length; i++){
+    if (searchElement === list[i]) {
+      count++;
+    }
+  }
+  return (count);
+};
