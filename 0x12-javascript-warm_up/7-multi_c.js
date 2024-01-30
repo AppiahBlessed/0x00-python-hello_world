a8c99c690365c1faeb0bea7ba24aa777f77d291b
Appiah Blessed <kwabenadicey@gmail.com>
2024-01-30 13:59:49 +0000

    Removed dormant line of code

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,13 +5,11 @@
 const num = process.argv[2];
 
 if (process.argv.length !== 3) {
-	console.log("Missing number of occurrences");
+  console.log("Missing number of occurrences");
 }
 
 if (num >= 0) {
-	for (let i = 0; i < num; i++) {
-		console.log("C is fun");
-//} else {
-//	console.log("");
+  for (let i = 0; i < num; i++) {
+  console.log("C is fun");
 }
 }
