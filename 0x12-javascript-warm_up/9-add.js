8364d3dae0d46660d394126a204a5a9ae7d4b1bb
Appiah Blessed <kwabenadicey@gmail.com>
2024-01-30 14:13:08 +0000

    Fixed function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,14 +6,14 @@
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
-function add(a, b) {
-	a = num1;
-	b = num2;
-// Checking if arg is a number
-if (isNaN(a) || isNaN(b)) {
-  console.log("NaN");
-} else {
-  console.log(a + b);
+function add (a, b) {
+  a = num1;
+  b = num2;
+  // Checking if arg is a number
+  if (isNaN(a) || isNaN(b)) {
+    console.log('NaN');
+  } else {
+    console.log(a + b);
 }
 }
 // Calling the function
