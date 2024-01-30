7aee3cc895d8954caa26b0cdb0dc7db83eed3416
Appiah Blessed <kwabenadicey@gmail.com>
2024-01-30 13:51:51 +0000

    Print list items

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,7 +3,7 @@
 * This script prints a multi line string
 */
 
-const lists = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const lists = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < lists.length; i++) {
-	console.log(lists[i] + '\n');
+  console.log(lists[i]);
 }
