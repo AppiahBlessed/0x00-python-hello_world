e590d0ad5535ca4393c54e87e1ee8679088efe49
Appiah Blessed <kwabenadicey@gmail.com>
2024-01-30 21:23:01 +0000

    Log previous call of function

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,9 +3,9 @@
 * This script reverses a list
 */
 exports.esrever = function (list) {
-  let newList = [];
+  const newList = [];
   for (let i = list.length - 1; i >= 0; i--) {
     newList.push(list[i]);
-	}
+  }
   return (newList);
 };
