cab7e9b82478e3bee29c03488363a319a239a9af
Appiah Blessed <kwabenadicey@gmail.com>
2024-01-30 16:39:43 +0000

    Semmistandard compliance

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,12 +3,12 @@
 * The continuation of the Rectangle class
 */
 class Rectangle {
-  constructor(w, h){
+  constructor (w, h) {
     if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
   }
-};
+}
 
-module.exports = Rectangle
+module.exports = Rectangle;
